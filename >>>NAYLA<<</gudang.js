1069fb696f9f996ecd2a43eb91abbf19e3bc130a
ZansLord <shikaizaidan@gmail.com>
2021-11-12 09:34:06 +0700

    botzindo

---
+++ >>>NAYLA<<</gudang.js
@@ -0,0 +1,86 @@
+{	
+	"prefix": "$",
+
+		
+	"kelas": "8 SMP",
+	
+	
+	"gender": "CWOK",
+	
+	
+	"agama": "ISLAM",
+	
+	
+	"asal": "KAB.BANDUNG",
+	
+	
+	"umur": "14 TAHUN",
+	
+	
+	"contoh": "INDONESIA",
+	
+		
+	"donasi": "CIEE MAU DONASI YA BANG?🗿",
+	
+	
+	"pulsa": "YOUR_NOMER",
+	
+	
+	"gopay": "YOUR_GOPAY",
+	
+	
+	"ovo": "YOUR_OVO",
+	
+	
+	"dana": "YOUR_DANA",
+	
+	
+	"namabot": "RIMURUBOTZ",
+	
+	
+	"NamaOwner": "ZANSLORD",
+	
+	
+	"MenuWm": "RECODE BY ZANSLORD",	
+	
+	
+	"ownerNumber": "62812874133914",
+	
+	
+	"NumberOwner": "+62 812-8741-33914",
+	
+	
+	"botNumber": "62812874133916",
+	
+	
+	"note": "HAII KAK, SALAM KENAL",
+	
+	
+	"VerifyBot": "I'M FROM INDONESIA",
+	
+	
+	"LolhumanApi": "3a5fadc9dfb7b4b1f2a5a8e3",
+	
+	
+	"xteamApi": "170342e43e147afa",
+	
+	
+	"replyproses": "[❗] SEDANG DIPROSES",
+	
+	
+    "NaySend": "NIHH KAK, JGN LUPA FOLLOW IG instagram.com/black_team_indonesia",
+	
+	
+    "linkyt": "GITHUB AE https://github.com/ZansLord",
+	
+	
+    "grub1": "https://chat.whatsapp.com/LTL9J5K0dxp65WspruIQuW",
+	
+	
+    "grub2": "https://chat.whatsapp.com/EAchc4mIgm6D01ViRSSz2b",
+	
+	
+    "grub3": "https://chat.whatsapp.com/HqQ9JkIzCAc21EpnPEiMT8"
+	
+	
+}
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+ 
+apt-get update
+apt-get upgrade
+npm i nodejs
+npm i brainly-scraper
+npm i libwebp
+apt-get install mc
+npm i ffmpeg
+npm i wget
+npm i marker
+npm i tesseract
+npm i speed
+npm i imgbb-uploader
+npm i axios
+npm i moment-timezone
+npm i fluent-ffmpeg
+npm i tiktok-scraper
+npm i node-fetch
+npm i @kagchi/kag-api
+npm i child_process
+npm i util
+npm i fs
+npm i crypto
+npm i @adiwajshing/baileys
+npm i request
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+ 
+echo "[*] di atas ada yang error kan?? itu masalah biasa. silahkan ketik npm start. dan subscribe yt RIMURUBOTZ"
